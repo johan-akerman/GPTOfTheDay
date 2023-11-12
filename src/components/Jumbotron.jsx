8d76f3d65c1f3c0b7f712d9e7b6b8a556fbb113d
johan-akerman <joha.aker@gmail.com>
2023-11-12 17:00:36 +0100

    fixed non responsive jumbotron on small devices

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -5,7 +5,7 @@ export function Jumbotron() {
   return (
     <>
       <main className="bg-mediumBrown">
-        <div className="md:w-9/12 md:text-left text-center w-11/12 pt-12 pb-20 grid grid-cols-12 gap-10 mx-auto">
+        <div className="md:w-9/12 md:text-left w-11/12 pt-12 pb-20 grid grid-cols-12 gap-10 mx-auto">
           <div className="lg:col-span-7 col-span-11 md:pt-20 pt-0">
             <h1 className="text-darkBrown md:text-7xl text-5xl font-bold mb-6">
               Discover, launch and vote on the best GPTs.
